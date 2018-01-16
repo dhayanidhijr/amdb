67ee1fed1fbfa823f20374d6183abb8a4af6ae06
Dhayanidhi Jayaraman <dhayajr@dhayanidhis-mbp.local>
2018-01-16 14:26:17 -0500

    Contract and web3 interface implemented

--- src/modules/am-new/index.js
+++ src/modules/am-new/index.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
+import AMNewContract from './am-new-contract.sol';
+//  import solc from 'solc';
+import { web3 } from '../../web3';
 
 class AMNew extends Component {
-  render() {
-    return (
-      <div>
-          Enter Auto mobile details
-          <div>
-              Make : <input type = "text" />
-              Model : <input type = "text" />
-              <input type = "button" value = "Deploy" />
-          </div>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            amNewContract: undefined,
+            compiledAMNewContract : undefined
+        }
+    }
+    
+    componentWillMount() {
+        this.readAMNewContract(AMNewContract);
+    }
+
+    readAMNewContract(contractFile) {
+        const rawFile = new XMLHttpRequest();
+        rawFile.open("GET", contractFile, false);
+        rawFile.onreadystatechange = () => {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status === 0)
+                {
+                    this.setState({
+                        amNewContract: rawFile.responseText
+                    }, () => {
+                        this.setState({
+                            //  compiledAMNewContract: solc.compile(this.amNewConctract(), 1)
+                            compiledAMNewContract: "solc.compile(this.amNewConctract(), 1)"
+                        });
+                    });
+                }
+            }
+        }
+        rawFile.send(null);
+    }
+
+    amNewConctract() {
+        return this.state.amNewContract;
+    }
+
+    compiledAMNewContract() {
+        return this.state.compiledAMNewContract;
+    }
+    
+    render() {
+        return (
+        <div>
+            Enter Auto mobile details
+            <div>
+                Make : <input type = "text" />
+                Model : <input type = "text" />
+                IsConnected : {web3.isConnected().toString()}
+                <textarea>{this.compiledAMNewContract()}</textarea>
+                <input type = "button" value = "Deploy" />
+                {this.amNewConctract()}
+
+            </div>
+        </div>
+        );
+    }
 }
 
 export default AMNew;
